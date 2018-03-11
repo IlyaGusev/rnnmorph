84331eba4f28ac0b7fe4bf623e372883ca6950b2
Ilya Gusev <phoenixilya@gmail.com>
2018-03-12 01:53:56 +0300

    Повысил версию

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import find_packages, setup
 setup(
     name='rnnmorph',
     packages=find_packages(),
-    version='0.2.0',
+    version='0.2.1',
     description='RNNMorph: neural network disambiguation of pymorphy2 parses for precise '
                 'POS-tagging in Russian language.',
     author='Ilya Gusev',
     author_email='phoenixilya@gmail.com',
     url='https://github.com/IlyaGusev/rnnmorph',
-    download_url='https://github.com/IlyaGusev/rnnmorph/archive/0.2.0.tar.gz',
+    download_url='https://github.com/IlyaGusev/rnnmorph/archive/0.2.1.tar.gz',
     keywords=['nlp', 'russian', 'lstm', 'morphology'],
     package_data={
         'rnnmorph': ['models/*']
@@ -19,12 +19,11 @@ setup(
         'scipy>=0.18.1',
         'scikit-learn>=0.18.1',
         'tensorflow>=1.1.0',
-        'keras==2.0.5',
+        'keras>=2.0.6',
         'pymorphy2>=0.8',
         'russian-tagsets==0.6',
         'tqdm>=4.14.0',
-        'jsonpickle>=0.9.4',
-        'h5py == 2.7.0'
+        'jsonpickle>=0.9.4'
     ],
     classifiers=[
         'Development Status :: 4 - Beta',
